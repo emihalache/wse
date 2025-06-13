78abed71b80550fba9e6f2ccce0a09b3ee4ba6df
elena <E.mihalache@student.tudelft.nl>
2025-06-13 14:04:59 +0200

    Add clustering

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def main():
     logger.info(f"New shape of the df: {new_df.shape}")
 
     # Start analysing the data
-    analyzer.sub1(new_df)               # This gets the nr. of releases per year
+    analyzer.sub3(new_df)               # This gets the nr. of releases per year
 
 if __name__ == '__main__':
     main()
